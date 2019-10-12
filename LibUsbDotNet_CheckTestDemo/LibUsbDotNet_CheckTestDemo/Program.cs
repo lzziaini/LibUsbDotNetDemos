f70631d500b448c5df0701d4af905b147757f88e
Zachary.huang <Zachary.huang@cygia.com>
2019-10-12 15:24:51 +0800

    2019-10-12 15:24:43

---
+++ LibUsbDotNet_CheckTestDemo/LibUsbDotNet_CheckTestDemo/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LibUsbDotNet;
+using LibUsbDotNet.Main;
+using LibUsbDotNet.Info;
+using System.Collections.ObjectModel;
+
+namespace LibUsbDotNet_CheckTestDemo
+{
+    class Program
+    {
+        public static void PrintUsbInfo()
+        {
+            UsbDevice usbDevice = null;
+            UsbRegDeviceList allDevices = UsbDevice.AllDevices;
+
+            Console.WriteLine("Found {0} devices", allDevices.Count);
+
+            foreach (UsbRegistry usbRegistry in allDevices)
+            {
+                Console.WriteLine("Got device: {0}\r\n", usbRegistry.FullName);
+
+                if (usbRegistry.Open(out usbDevice))
+                {
+                    Console.WriteLine("Device Information\r\n------------------");
+
+                    Console.WriteLine("{0}", usbDevice.Info.ToString());
+
+                    Console.WriteLine("VID & PID: {0} {1}", usbDevice.Info.Descriptor.VendorID, usbDevice.Info.Descriptor.ProductID);
+
+                    Console.WriteLine("\r\nDevice configuration\r\n--------------------");
+                    foreach (UsbConfigInfo usbConfigInfo in usbDevice.Configs)
+                    {
+                        Console.WriteLine("{0}", usbConfigInfo.ToString());
+
+                        Console.WriteLine("\r\nDevice interface list\r\n---------------------");
+                        ReadOnlyCollection<UsbInterfaceInfo> interfaceList = usbConfigInfo.InterfaceInfoList;
+                        foreach (UsbInterfaceInfo usbInterfaceInfo in interfaceList)
+                        {
+                            Console.WriteLine("{0}", usbInterfaceInfo.ToString());
+
+                            Console.WriteLine("\r\nDevice endpoint list\r\n--------------------");
+                            ReadOnlyCollection<UsbEndpointInfo> endpointList = usbInterfaceInfo.EndpointInfoList;
+                            foreach (UsbEndpointInfo usbEndpointInfo in endpointList)
+                            {
+                                Console.WriteLine("{0}", usbEndpointInfo.ToString());
+                            }
+                        }
+                    }
+                    usbDevice.Close();
+                }
+                Console.WriteLine("\r\n----- Device information finished -----\r\n");
+            }
+        }
+        static void Main(string[] args)
+        {
+            PrintUsbInfo();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LibUsbDotNet_ShowTestDemo/LibUsbDotNet_ShowTestDemo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using LibUsbDotNet;
+using LibUsbDotNet.Info;
+using LibUsbDotNet.Main;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace LibUsbDotNet_ShowTestDemo
+{
+    class Program
+    {
+        public static UsbDevice MyUsbDevice;
+
+        public static void Main(string[] args)
+        {
+            // Dump all devices and descriptor information to console output.
+            UsbRegDeviceList allDevices = UsbDevice.AllDevices;
+            Debug.WriteLine(allDevices.Count);
+            foreach (UsbRegistry usbRegistry in allDevices)
+            {
+                if (usbRegistry.Open(out MyUsbDevice))
+                {
+                    Console.WriteLine(MyUsbDevice.Info.ToString()+"--------1-----------");
+                    for (int iConfig = 0; iConfig < MyUsbDevice.Configs.Count; iConfig++)
+                    {
+                        UsbConfigInfo configInfo = MyUsbDevice.Configs[iConfig];
+                        Console.WriteLine(configInfo.ToString()+"---------2-----------");
+
+                        ReadOnlyCollection<UsbInterfaceInfo> interfaceList = configInfo.InterfaceInfoList;
+                        for (int iInterface = 0; iInterface < interfaceList.Count; iInterface++)
+                        {
+                            UsbInterfaceInfo interfaceInfo = interfaceList[iInterface];
+                            Console.WriteLine(interfaceInfo.ToString()+"--------3-------");
+
+                            ReadOnlyCollection<UsbEndpointInfo> endpointList = interfaceInfo.EndpointInfoList;
+                            for (int iEndpoint = 0; iEndpoint < endpointList.Count; iEndpoint++)
+                            {
+                                Console.WriteLine(endpointList[iEndpoint].ToString()+"-------4--------");
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            // Free usb resources.
+            // This is necessary for libusb-1.0 and Linux compatibility.
+            UsbDevice.Exit();
+
+            // Wait for user input..
+            Console.ReadKey();
+        }
+    }
+}
